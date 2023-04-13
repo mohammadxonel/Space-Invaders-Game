6405204281e279059094ccbb3fe7e22394e35c94
moahammadxxali <ali_mohammad@t-online.de>
2023-04-13 23:57:29 +0200

    add js app

---
+++ app.js
@@ -0,0 +1,10 @@
+const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+let aliensRemove = []
+let results = 0
+
