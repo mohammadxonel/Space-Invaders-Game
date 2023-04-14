6092a3346e9a3d9c97426daeeedada1983f5d5fa
moahammadxxali <ali_mohammad@t-online.de>
2023-04-14 22:48:21 +0200

    add js app edits to moveShooter

--- app.js
+++ app.js
@@ -39,4 +39,18 @@ const alienInvaders = [
 
   square[currentShooterIndex].classList.add('shooter')
 
-  
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+      case 'ArrowLeft':
+        if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+        break
+      case 'ArrowRight' :
+        if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+        break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+  }
+  document.addEventListener('keydown', moveShooter)
+
+
