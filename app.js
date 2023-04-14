922cb4d4224fff15e64ac22290bf24a5ae1d8ab5
moahammadxxali <ali_mohammad@t-online.de>
2023-04-14 20:10:42 +0200

    add js app edits to draw

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ for (let i = 0; i < 225; i++) {
 
 const square = Array.from(document.querySelectorAll('.grid div'))
 
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,10,
+    15,16,17,18,19,20,21,22,23,24,25,
+    30,31,32,33,34,35,36,37,38,39,40,
+  ]
+
+  function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        if(!aliensRemove.includes(i)) {
+            square[alienInvaders[i]].classList.add('invader')
+        }
+    }
+  }
+
+  draw()
+
+  
